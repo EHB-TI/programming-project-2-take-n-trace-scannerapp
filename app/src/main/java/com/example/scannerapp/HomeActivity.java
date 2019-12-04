dbd7212324ab300b031331a5e8d2190cd560aa5a
Imanuel <mrjsaw@gmail.com>
2019-12-04 13:25:17 +0100

    Changed main menu layout + added dispatch call

--- app/src/main/java/com/example/scannerapp/HomeActivity.java
+++ app/src/main/java/com/example/scannerapp/HomeActivity.java
@@ -3,6 +3,7 @@ package com.example.scannerapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -18,10 +19,16 @@ public class HomeActivity extends AppCompatActivity {
 
     }
 
-    public void onClickBtn(View v)
+    public void startScan(View v)
     {
         Toast.makeText(this, "Clicked on Button", Toast.LENGTH_LONG).show();
         Intent myIntent = new Intent(HomeActivity.this, MainActivity.class);
         HomeActivity.this.startActivity(myIntent);
+    }
+    public void callDispatch(View v)
+    {
+        String phone = "+32488389575"; //Dispatch phone number here
+        Intent intent = new Intent(Intent.ACTION_DIAL, Uri.fromParts("tel", phone, null));
+        startActivity(intent);
     }
 }
