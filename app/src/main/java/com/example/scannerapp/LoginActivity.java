648207bc9ecf3c3547d3c580555f3327b3cd5b34
Imanuel <mrjsaw@gmail.com>
2019-12-10 11:13:43 +0100

    added Auth0 dependencies, manifestPlaceholders, Auth0 object in LoginActivity

--- app/src/main/java/com/example/scannerapp/LoginActivity.java
+++ app/src/main/java/com/example/scannerapp/LoginActivity.java
@@ -7,14 +7,18 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import com.auth0.android.Auth0;
+
 import static android.view.View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN;
 
 public class LoginActivity extends AppCompatActivity {
 
+    Auth0 account;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        account = new Auth0(this);
         View decorView = getWindow().getDecorView();
 // Hide the status bar.
         int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
